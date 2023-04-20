afb10f4ce2d960f86704450083ecc08c6912d72e
Eric DiGiovine <edigiovine75@gmail.com>
2023-04-20 14:56:50 -0700

    saving work

--- src/main/java/com/storyengine/Imaghur.java
+++ src/main/java/com/storyengine/Imaghur.java
@@ -79,18 +79,8 @@ public class Imaghur {
                 int y1 = (int) (Math.random() * imageHeight);
                 Point pix = new Point(x1, y1);
 
-                // Gather pixel neighbors
-                ArrayList<Point> neighborsList = gatherPixelNeighbors(pix);
-
-                // Gather pixels found in neighbors list if they can accept color and were NOT already visited
-                for (Point p : neighborsList) {
-                    if (!isOutOfBounds(outputImage, p.x, p.y) && outputImage.getRGB(p.x, p.y) == Color.WHITE.getRGB()
-                            && !pixelsToPaint.contains(p) && !visitedPixels.contains(p)) {
-                        pixelsToPaint.add(p);
-                        visitedPixels.add(p);
-                        numPixelsToPaint++;
-                    }
-                }
+                // Gather pixel and its neighbors in one list
+                ArrayList<Point> neighborsList = gatherPixelNeighbors(pix, givenColor.getValue(), outputImage);
             }
 
             // Now paint each gathered pixel the same color
@@ -107,25 +97,75 @@ public class Imaghur {
         ImageIO.write(outputImage, "png", outputFile);
     }
 
-    public static ArrayList<Point> gatherPixelNeighbors(Point point) {
+    public static ArrayList<Point> gatherPixelNeighbors(Point point, Integer threshold, BufferedImage imageTemplate) {
         // Create list containing initial pixel and its immediate neighbors
         ArrayList<Point> pixelNeighbors = new ArrayList<>();
-        pixelNeighbors.add(point);
-        pixelNeighbors.add(new Point(point.x+1, point.y));
-        pixelNeighbors.add(new Point(point.x-1, point.y));
-        pixelNeighbors.add(new Point(point.x, point.y+1));
-        pixelNeighbors.add(new Point(point.x, point.y-1));
-        pixelNeighbors.add(new Point(point.x+1, point.y+1));
-        pixelNeighbors.add(new Point(point.x+1, point.y-1));
-        pixelNeighbors.add(new Point(point.x-1, point.y+1));
-        pixelNeighbors.add(new Point(point.x-1, point.y-1));
-        return pixelNeighbors;
-    }
+        int gatheredPixels = 0;
 
-    public static boolean isOutOfBounds(BufferedImage image, int x, int y) {
-        int width = image.getWidth();
-        int height = image.getHeight();
-        return x < 0 || x >= width || y < 0 || y >= height;
+        // Add original point to our list once
+        pixelNeighbors.add(point);
+
+        // Only gather pixels until we reach our threshold
+        while (gatheredPixels < threshold) {
+            // Add pixels to neighbors list if adjacent to original pixel and not already in the list
+            // pixr = pixel to the right of point
+            Point pixr = new Point(point.x+1, point.y);
+            if (pixr.x >= 0 && pixr.x < imageTemplate.getWidth() && pixr.y >= 0 && pixr.y < imageTemplate.getHeight()
+                    && !pixelNeighbors.contains(pixr)) {
+                pixelNeighbors.add(pixr);
+                gatheredPixels++;
+            }
+            // pixl = pixel to the left of point
+            Point pixl = new Point(point.x-1, point.y);
+            if (pixl.x >= 0 && pixl.x < imageTemplate.getWidth() && pixl.y >= 0 && pixl.y < imageTemplate.getHeight()
+                    && !pixelNeighbors.contains(pixl)) {
+                pixelNeighbors.add(pixl);
+                gatheredPixels++;
+            }
+            // pixu = pixel above the point
+            Point pixu = new Point(point.x, point.y+1);
+            if (pixu.x >= 0 && pixu.x < imageTemplate.getWidth() && pixu.y >= 0 && pixu.y < imageTemplate.getHeight()
+                    && !pixelNeighbors.contains(pixu)) {
+                pixelNeighbors.add(pixu);
+                gatheredPixels++;
+            }
+            // pixd = pixel below the point
+            Point pixd = new Point(point.x, point.y-1);
+            if (pixd.x >= 0 && pixd.x < imageTemplate.getWidth() && pixd.y >= 0 && pixd.y < imageTemplate.getHeight()
+                    && !pixelNeighbors.contains(pixd)) {
+                pixelNeighbors.add(pixd);
+                gatheredPixels++;
+            }
+            // pixur = pixel to the upper right of point
+            Point pixur = new Point(point.x+1, point.y+1);
+            if (pixur.x >= 0 && pixur.x < imageTemplate.getWidth() && pixur.y >= 0 && pixur.y < imageTemplate.getHeight()
+                    && !pixelNeighbors.contains(pixur)) {
+                pixelNeighbors.add(pixur);
+                gatheredPixels++;
+            }
+            // pixul = pixel to the upper left of point
+            Point pixul = new Point(point.x+1, point.y-1);
+            if (pixul.x >= 0 && pixul.x < imageTemplate.getWidth() && pixul.y >= 0 && pixul.y < imageTemplate.getHeight()
+                    && !pixelNeighbors.contains(pixul)) {
+                pixelNeighbors.add(pixul);
+                gatheredPixels++;
+            }
+            // pixdr = pixel to the lower right of point
+            Point pixdr = new Point(point.x-1, point.y+1);
+            if (pixdr.x >= 0 && pixdr.x < imageTemplate.getWidth() && pixdr.y >= 0 && pixdr.y < imageTemplate.getHeight()
+                    && !pixelNeighbors.contains(pixdr)) {
+                pixelNeighbors.add(pixdr);
+                gatheredPixels++;
+            }
+            // pixdr = pixel to the lower left of point
+            Point pixdl = new Point(point.x-1, point.y-1);
+            if (pixdl.x >= 0 && pixdl.x < imageTemplate.getWidth() && pixdl.y >= 0 && pixdl.y < imageTemplate.getHeight()
+                    && !pixelNeighbors.contains(pixdl)) {
+                pixelNeighbors.add(pixdl);
+                gatheredPixels++;
+            }
+        }
+        return pixelNeighbors;
     }
 }
 
